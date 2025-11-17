462592c9e2ee8237156cdde6f1691d73fe0975b4
Kevin <kcrapo@csumb.edu>
2025-11-17 11:57:22 -0800

    Database Setup
    Set up a new database that I am hosting via heroku. Created preliminary tables and added the first user account manually using SQL. Refer to sql in slack to see how it was populated.

--- index.mjs
+++ index.mjs
@@ -11,10 +11,10 @@ app.use(express.urlencoded({extended:true}));
 
 //setting up database connection pool
 const pool = mysql.createPool({
-    host: "your_hostname",
-    user: "your_username",
-    password: "your_password",
-    database: "your_database",
+    host: "ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "t21fpdn4xxyrg99i",
+    password: "xf9ey02a1hyzbkyi",
+    database: "bdptrv7p6sbh1fn7",
     connectionLimit: 10,
     waitForConnections: true
 });
